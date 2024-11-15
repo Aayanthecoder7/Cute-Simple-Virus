da883810a71356bf51c71cc881d34b42ab8e1889
Aayan Ahmad Khan <120969593+Aayanthecoder7@users.noreply.github.com>
2024-11-15 18:55:55 +0100

    Add files via upload

---
+++ K-768.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw()
+
+messagebox.showerror("Virus Alert!", "Hi, I am an Albanian Virus but because of poor technology in my country unfortunately I am not able to harm your computer. Please be so kind to delete one of your important files yourself and then forward me to other users. Many thanks for your cooperation! Best regards,Albanian virus")
+
+root.mainloop()
